12b56a3c95950672da0b18c45e82394b98b8d7bc
Benjamin Crozat <benjamincrozat@me.com>
2021-04-26 07:01:55 +0200

    Clean up

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,15 @@ use App\Http\Controllers\Likes\ListLikesController;
 use App\Http\Controllers\Likes\SearchLikesController;
 
 Route::get('/', HomeController::class)->name('home');
-
-Route::get('/login', [SocialiteController::class, 'redirectToProvider'])->name('login');
-Route::get('/login/callback', [SocialiteController::class, 'handleProviderCallback'])->name('login.callback');
-Route::post('/logout', [SocialiteController::class, 'logout'])->name('logout');
-
 Route::get('/blocked', BlockedController::class)->name('blocked');
 Route::get('/followers', FollowersController::class)->name('followers');
 Route::get('/followings', FollowingsController::class)->name('followings');
 Route::get('/likes', ListLikesController::class)->name('likes.index');
 Route::get('/likes/search', SearchLikesController::class)->name('likes.search');
+Route::get('/login', [SocialiteController::class, 'redirectToProvider'])->name('login');
+Route::get('/login/callback', [SocialiteController::class, 'handleProviderCallback'])->name('login.callback');
+Route::post('/logout', [SocialiteController::class, 'logout'])->name('logout');
 Route::get('/muted', MutedController::class)->name('muted');
 Route::get('/overview', OverviewController::class)->name('overview');
 Route::get('/settings', SettingsController::class)->name('settings');
-
 Route::delete('/user', DeleteUserController::class)->name('user.delete');
