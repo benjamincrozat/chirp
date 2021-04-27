79287dc587d0602fc7729f0942a61f0025b91a03
Benjamin Crozat <benjamincrozat@me.com>
2021-04-27 06:17:59 +0200

    I’m dumb sometimes
    The “guest” middleware redirect to home. So it was an infinite loop.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
     public function __invoke(Request $request)
     {
+        if (Auth::check()) {
+            return Redirect::route('overview');
+        }
+
         return view('home');
     }
 }
