f447683c5ef79d34fa1a03078d6b35451a5bece4
Benjamin Crozat <benjamincrozat@me.com>
2021-04-27 09:39:51 +0200

    This shouldn’t be in the main code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-use App\Twitter\TwitterOAuthWithCache;
+use Abraham\TwitterOAuth\TwitterOAuth;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -16,10 +16,27 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         app()->bind('twitter', function (Application $app) {
-            return new TwitterOAuthWithCache(
-                $app['config']->get('services.twitter.client_id'),
-                $app['config']->get('services.twitter.client_secret'),
-            );
+            return new class($app['config']->get('services.twitter.client_id'), $app['config']->get('services.twitter.client_secret')) extends TwitterOAuth {
+                public function get($path, array $parameters = [])
+                {
+                    $key = "{$path}_" . md5(serialize($parameters));
+
+                    if (cache()->has($key)) {
+                        $this->response->setHttpCode(200);
+
+                        return cache()->get($key);
+                    }
+
+                    $response = parent::get($path, $parameters);
+
+                    // We cache the response only if we don't have errors.
+                    if (empty($response->errors)) {
+                        cache()->put($key, $response);
+                    }
+
+                    return $response;
+                }
+            };
         });
     }
 }
